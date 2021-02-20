a22f738b0712633c9086df1dfb1a1f0fcc7064a4
nish616 <nishin616@gmail.com>
2021-02-20 19:54:51 +0530

    Add coupon not found

--- routes/validateCoupon.js
+++ routes/validateCoupon.js
@@ -10,29 +10,38 @@ router.post("/validateCoupon", async (req,res) => {
 
     const coupon = await Coupon.findOne({name : couponName});
 
-    const { isActive, end, isPercent, percent, minAmount, maxAmount} = coupon;
-
-    if(!isActive){
-        return res.status(400).json({"message" : "Coupon expired!"});
-    } else if(date > end){
-        return res.status(400).json({"message" : "Coupon expired!"});
-    } else if(cartTotal < minAmount){
-        return res.status(400).json({"message" : `Cart total should have minimum ${minAmount}`});
-    } else if(isPercent){
-        discount = (cartTotal) * (percent/100);
-        if(discount >= maxAmount ){
-           // newCartTotal = cartTotal - maxAmount;
-           discount = maxAmount;
-            return res.status(200).json({"message" : "Coupon Applied!","Discount" : discount});
-        }else {
-            //newCartTotal = cartTotal - newCartTotal;
-            return res.status(200).json({"message" : "Coupon Applied!","Discount" : discount});
-        }
-    } else {
-        discount =  maxAmount;
-        
-        res.status(200).json({"message" : "Coupon Applied!","Discount" : discount});
+    if(coupon == null){
+        return res.status(404).json({"message" : "Coupon not found!" });
     }
+    else {
+
+        const { isActive, end, isPercent, percent, minAmount, maxAmount} = coupon;
+
+        if(!isActive){
+            return res.status(400).json({"message" : "Coupon expired!"});
+        } else if(date > end){
+            return res.status(400).json({"message" : "Coupon expired!"});
+        } else if(cartTotal < minAmount){
+            return res.status(400).json({"message" : `Cart total should have minimum ${minAmount}`});
+        } else if(isPercent){
+            discount = (cartTotal) * (percent/100);
+            if(discount >= maxAmount ){
+            // newCartTotal = cartTotal - maxAmount;
+            discount = maxAmount;
+                return res.status(200).json({"message" : "Coupon Applied!","Discount" : discount});
+            }else {
+                //newCartTotal = cartTotal - newCartTotal;
+                return res.status(200).json({"message" : "Coupon Applied!","Discount" : discount});
+            }
+        } else {
+            discount =  maxAmount;
+            
+            res.status(200).json({"message" : "Coupon Applied!","Discount" : discount});
+        }
+
+    }
+
+    
 
  }catch(err){
     return res.status(400).send("Coupon invalid");
